4bf81a7c7036e44cc4ed678f50d37dbdf275759b
denniszielke <11569044+denniszielke@users.noreply.github.com>
2024-06-19 11:47:45 +0200

    Added phase4 src code

---
+++ src-agents/phase4/main.py
@@ -0,0 +1,96 @@
+import os
+import json
+import requests
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from pydantic import BaseModel
+from enum import Enum
+from openai import AzureOpenAI
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
+import redis
+
+app = FastAPI()
+
+load_dotenv()
+
+class QuestionType(str, Enum):
+    multiple_choice = "multiple_choice"
+    true_or_false = "true_or_false"
+    popular_choice = "popular_choice"
+    estimation = "estimation"
+
+class Ask(BaseModel):
+    question: str | None = None
+    type: QuestionType
+    correlationToken: str | None = None
+
+class Answer(BaseModel):
+    answer: str
+    correlationToken: str | None = None
+    promptTokensUsed: int | None = None
+    completionTokensUsed: int | None = None
+
+client: AzureOpenAI
+
+if "AZURE_OPENAI_API_KEY" in os.environ:
+    client = AzureOpenAI(
+        api_key = os.getenv("AZURE_OPENAI_API_KEY"),  
+        api_version = os.getenv("AZURE_OPENAI_VERSION"),
+        azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+    )
+else:
+    token_provider = get_bearer_token_provider(DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default")
+    client = AzureOpenAI(
+        azure_ad_token_provider=token_provider,
+        api_version = os.getenv("AZURE_OPENAI_VERSION"),
+        azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT"),
+    )
+
+deployment_name = os.getenv("AZURE_OPENAI_COMPLETION_DEPLOYMENT_NAME")
+index_name = "movies-semantic-index"
+service_endpoint = os.getenv("AZURE_AI_SEARCH_ENDPOINT")
+model_name = os.getenv("AZURE_OPENAI_COMPLETION_MODEL")
+
+# Redis connection details
+redis_host = os.getenv('REDIS_HOST')
+redis_port = os.getenv('REDIS_PORT')
+redis_password = os.getenv('REDIS_PASSWORD')
+ 
+# Connect to the Redis server
+conn = redis.Redis(host=redis_host, port=redis_port, password=redis_password, encoding='utf-8', decode_responses=True)
+ 
+if conn.ping():
+    print("Connected to Redis")
+
+@app.get("/")
+async def root():
+    return {"message": "Hello Smorgs"}
+
+@app.get("/healthz", summary="Health check", operation_id="healthz")
+async def get_products(query: str = None):
+    """
+    Returns a status of the app
+    """
+
+@app.post("/ask", summary="Ask a question", operation_id="ask") 
+async def ask_question(ask: Ask):
+    """
+    Ask a question
+    """
+
+    # Send a completion call to generate an answer
+    print('Sending a request to openai')
+    start_phrase = ask.question
+    response = client.chat.completions.create(
+        model = deployment_name,
+        messages = [{"role" : "assistant", "content" : start_phrase}],
+    )
+
+    print(response.choices[0].message.content)
+    print(response)
+    answer = Answer(answer=response.choices[0].message.content)
+    answer.correlationToken = ask.correlationToken
+    answer.promptTokensUsed = response.usage.prompt_tokens
+    answer.completionTokensUsed = response.usage.completion_tokens
+
+    return answer
